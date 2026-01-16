51a71159c16c6f357a11bf6d39e95e0fb91ad14a
astro-pavan <pt426@cam.ac.uk>
2026-01-16 15:19:35 +0000

    Minor changes

--- tests/test_planet.py
+++ tests/test_planet.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../src'))
 from kamino.planet import planet
 from kamino.constants import *
 
-p1 = planet(1e5, 3000, 1.0)
+p1 = planet(1e5, 3000, 0.5)
 
 p1.find_steady_state(2e5)
 
