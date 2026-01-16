2114941b95d90ece7c489b94513479f584972b20
Pavan <pt426@cam.ac.uk>
2026-01-16 15:11:11 +0000

    Updated helios code

--- tests/test_helios.py
+++ tests/test_helios.py
@@ -3,7 +3,7 @@ import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../src'))
 
 from kamino.speedy_climate.climate import run_HELIOS
+from kamino.speedy_climate.interpolator import generate_data, plot_dataset
 from kamino.constants import *
 
-print(run_HELIOS('test', 1.3 * SOLAR_CONSTANT, 'G2', R_EARTH, M_EARTH, EARTH_ATM, 0.0004, 0.004, 0.05, 0.25, clouds=0.55, verbose=False, august_roche_magnus=True))
-
+print(run_HELIOS('test', 0.9 * SOLAR_CONSTANT, 'G2', R_EARTH, M_EARTH, EARTH_ATM, 1, 0.01, 0.05, 0.25, clouds=0.6, verbose=True, august_roche_magnus=True))
