60c2bba50a7a788eb8dc4e556c7f817a4ab36328
astro-pavan <pt426@cam.ac.uk>
2026-01-23 16:59:55 +0000

    Added backup CO2 calculation (not used)

--- src/kamino/ocean_chemistry/co2.py
+++ src/kamino/ocean_chemistry/co2.py
@@ -1,6 +1,8 @@
 from kamino.ocean_chemistry.aqueous_geochemistry import *
 from kamino.utils import *
 
+import PyCO2SYS as pyco2
+
 def get_P_CO2(P: float, T: float, alkalinity: float, DIC: float, Ca: float=0, Mg: float=0, Fe: float=0) -> float:
     """_summary_
 
@@ -39,6 +41,7 @@ def get_P_CO2(P: float, T: float, alkalinity: float, DIC: float, Ca: float=0, Mg
     #     return 0 # if the temperature is 0 C, the planet has frozen and no CO2 is in the atmosphere? 
 
     T = smooth_max(T, 273.5)
+    T = smooth_min(T, 500)
 
     input_lines = solution_block(P, T, composition, None) + output_block(saturation_indexes=['CO2(g)'])
     output = run_PHREEQC(input_lines, single_output=True)
@@ -47,3 +50,34 @@ def get_P_CO2(P: float, T: float, alkalinity: float, DIC: float, Ca: float=0, Mg
     CO2_SI = SI['CO2(g)']
 
     return (10 ** CO2_SI) * P
+
+def get_P_CO2_v2(P, T, alk, DIC):
+
+    known_alkalinity = alk * 1e6  # umol/kg (example ocean value)
+    known_dic = DIC * 1e6         # umol/kg (example ocean value)
+
+    temperature_surface = T + ABSOLUTE_ZERO # degrees Celsius
+    pressure_surface = 0     # dbar (0 dbar is roughly surface pressure)
+    salinity = 35            # PSU (Standard ocean salinity)
+
+    temperature_surface = smooth_max(0.5, temperature_surface)
+    temperature_surface = smooth_min(99.5, temperature_surface)
+
+    # --- The Calculation ---
+    results = pyco2.sys(
+        par1=known_alkalinity,  # Value of first parameter
+        par2=known_dic,         # Value of second parameter
+        par1_type=1,            # 1 tells code par1 is Alkalinity
+        par2_type=2,            # 2 tells code par2 is DIC
+        salinity=salinity,      # Required for equilibrium constants (K values)
+        temperature=temperature_surface,
+        pressure=pressure_surface
+    )
+
+    # --- Extracting the Answer ---
+    # The results dictionary contains calculated values for everything else.
+    # 'pCO2' is the partial pressure in micro-atmospheres (uatm).
+
+    pCO2_ocean = results['pCO2'] * EARTH_ATM
+
+    return pCO2_ocean
