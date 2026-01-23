c54351e8185582ad7676c0f158a09097324cc90d
astro-pavan <pt426@cam.ac.uk>
2026-01-23 17:00:24 +0000

    Test code

--- tests/test_planet.py
+++ tests/test_planet.py
@@ -5,11 +5,11 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../src'))
 from kamino.planet import planet
 from kamino.constants import *
 
-p1 = planet(1e5, 3000, 0.9)
+p1 = planet(1e5, 3000, 1.1)
 
-p1.run_simulation(1e7, True, [0.2, 0.2, 0.2, 0.2, 0.2, 0.2])
+# p1.run_simulation(1e3, True, [0.00005, 0.00005, 0.00005, 0.00005, 0.00005, 0.00005])
 
-# p1.find_steady_state(1e6)
+p1.find_steady_state(2000000)
 
 # df = p1.run_simulation(100)
 # df.to_csv('tests/test.csv')
--- tests/test_seafloor_weathering.py
+++ tests/test_seafloor_weathering.py
@@ -2,12 +2,23 @@ import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../src'))
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 # import kamino.seafloor_weathering.chili.plot_example
 
 from kamino.seafloor_weathering.weathering import *
 from kamino.constants import YR
 
-w_test = get_weathering_rate(1e5, 277, 0.02, 0.05 / YR, 100, 50e6 * YR)
+T_range = np.linspace(274, 340, num=20)
+W = []
+W2 = []
 
-print(w_test * YR)
+for T in T_range:
+    W.append(get_weathering_rate(1e5, T, 280 * 1e-6, 0.05, 100, 50e6))
+    W2.append(get_weathering_rate_old(1e5, T, 280 * 1e-6))
 
+plt.plot(T_range, W)
+plt.plot(T_range, W2)
+plt.yscale('log')
+plt.show()
