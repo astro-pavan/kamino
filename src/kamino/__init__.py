0455becb621da35e36facb604a5f461e800da33f
astro-pavan <pt426@cam.ac.uk>
2026-06-10 17:57:16 +0100

    Minor changes

--- src/kamino/__init__.py
+++ src/kamino/__init__.py
@@ -1,7 +1,7 @@
 from kamino.chemistry import (
-    elements, alk_idx, c_idx, ca_idx, mg_idx, si_idx,
+    elements, alk_idx, c_idx, ca_idx, mg_idx, si_idx, na_idx, cl_idx,
     get_P_CO2, get_pH, get_b_eq, get_k, solve_solution, ChemistryError,
 )
 from kamino.weathering import get_weathering_flux, get_continental_weathering_flux
 from kamino.precipitation import get_precipitation
-from kamino.mineral_info import basalt_composition, carbonate_minerals
+from kamino.mineral_info import carbonate_minerals
