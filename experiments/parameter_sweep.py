eb6a056959ceea56861686f10be73dcf4b372ef5
astro-pavan <pt426@cam.ac.uk>
2026-06-14 12:18:50 +0100

    Minor changes

--- experiments/parameter_sweep.py
+++ experiments/parameter_sweep.py
@@ -87,12 +87,12 @@ def run_sweep(instellation, outgassing, crust_production_rate, ocean_depth, reve
     print("All simulations complete.")
 
 
-instellation = [0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.05, 1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4]
+# instellation = [0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.05, 1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4]
 # outgassing = [0.01, 0.03, 0.1, 0.3, 1, 3, 10]
 # crust_production_rate = [0.01, 0.03, 0.1, 0.3, 1, 3, 10]
 # ocean_depth = [100, 300, 1000, 3000, 10000, 30000, 100000]
 
-# instellation = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4] # maybe stop at 1.2??
+instellation = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4] # maybe stop at 1.2??
 outgassing = [0.01, 0.03, 0.1, 0.3, 1, 3, 10]
 crust_production_rate = [0.01, 0.03, 0.1, 0.3, 1, 3, 10]
 ocean_depth = [300, 1000, 3000, 10000, 30000]
@@ -139,7 +139,7 @@ if __name__ == "__main__":
 
     # Sweep 1: basic
 
-    run_sweep(instellation, outgassing, crust_production_rate, ocean_depth_default, reverse_weathering_default, crust_composition_default, f_HT_default)
+    # run_sweep(instellation, outgassing, crust_production_rate, ocean_depth_default, reverse_weathering_default, crust_composition_default, f_HT_default)
 
     # Sweep 2: ocean depth
 
@@ -148,4 +148,8 @@ if __name__ == "__main__":
     # Sweep 3: crust composition
 
     # run_sweep(instellation, outgassing_default, crust_production_rate_default, ocean_depth_default, reverse_weathering_default, crust_composition, f_HT_default)
+
+    # Sweep 4: reverse weathering
+
+    run_sweep(instellation, outgassing_default, crust_production_rate_default, ocean_depth_default, reverse_weathering, crust_composition_default, f_HT_default)
 
